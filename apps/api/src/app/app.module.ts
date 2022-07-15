68c6864c9ef502235d3defc7b18018b77826602c
Mazedul Islam <mazidmailbox@gmail.com>
2022-07-15 07:18:50 +0600

    Connect to mongodb using mongoose

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ApplicationsModule } from '../applications/applications.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ApplicationsModule],
+  imports: [
+    ApplicationsModule,
+    MongooseModule.forRoot(
+      'mongodb://admin:pass1234@localhost:27017/jst?authMechanism=DEFAULT&authSource=admin'
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
