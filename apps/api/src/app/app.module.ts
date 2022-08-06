92fc13c513433333e3284e5de119301b82814ad2
Mazedul Islam <mazidmailbox@gmail.com>
2022-08-06 23:22:10 +0600

    Revert "wip: adding google auth, clientId, clientSecret added"
    This reverts commit 4cc83b67e0b7ff62088b2a33147b060e7f50e175.

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import * as Joi from 'joi';
 import { ApplicationsModule } from '../applications/applications.module';
 
 import { AppController } from './app.controller';
@@ -9,17 +7,10 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      validationSchema: Joi.object({
-        GOOGLE_AUTH_CLIENT_ID: Joi.string().required(),
-        GOOGLE_AUTH_CLIENT_SECRET: Joi.string().required(),
-      }),
-    }),
+    ApplicationsModule,
     MongooseModule.forRoot(
       'mongodb://admin:pass1234@localhost:27017/jst?authMechanism=DEFAULT&authSource=admin'
     ),
-    ApplicationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
