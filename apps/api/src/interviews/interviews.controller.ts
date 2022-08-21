bc03b9acf7f1f3942b7b0fd76e0585eeb95758be
Mazedul Islam <mazidmailbox@gmail.com>
2022-08-21 21:41:05 +0600

    Interviews service and controller implementation added

--- apps/api/src/interviews/interviews.controller.ts
+++ apps/api/src/interviews/interviews.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch } from '@nestjs/common';
+import { UpdateInterviewDto } from './dtos/update-interview.dto';
+import { InterviewsService } from './interviews.service';
 
 @Controller('interviews')
-export class InterviewsController {}
+export class InterviewsController {
+  constructor(private readonly interviewsService: InterviewsService) {}
+
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    return this.interviewsService.findById(id);
+  }
+
+  // @Post()
+  // create(@Body() interviewDto: CreateInterviewDto) {
+  //   return this.interviewsService.create(interviewDto);
+  // }
+
+  @Patch(':id')
+  update(@Param('id') id: string, interviewDto: UpdateInterviewDto) {
+    return this.interviewsService.update(id, interviewDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.interviewsService.delete(id);
+  }
+}
--- apps/api/src/interviews/interviews.service.ts
+++ apps/api/src/interviews/interviews.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateInterviewDto } from './dtos/create-interview.dto';
+import { UpdateInterviewDto } from './dtos/update-interview.dto';
+import { Interview } from './entities/interview.entity';
 
 @Injectable()
-export class InterviewsService {}
+export class InterviewsService {
+  constructor(
+    @InjectModel(Interview.name)
+    private readonly interviewModel: Model<Interview>
+  ) {}
+
+  async findById(id: string) {
+    const interview = await this.interviewModel.findById(id).exec();
+    if (!interview) {
+      throw new NotFoundException(`Interview #${id} not found`);
+    }
+    return interview;
+  }
+
+  create(interviewDto: CreateInterviewDto) {
+    const interview = new this.interviewModel(interviewDto);
+    return interview.save();
+  }
+
+  async update(id: string, interviewDto: UpdateInterviewDto) {
+    const updatedInterview = await this.interviewModel
+      .findByIdAndUpdate(id, interviewDto, { new: true })
+      .exec();
+    if (!updatedInterview) {
+      throw new NotFoundException(`Interview #${id} not found`);
+    }
+    return updatedInterview;
+  }
+
+  async delete(id: string) {
+    const result = this.interviewModel.findByIdAndDelete(id).exec();
+    if (!result) {
+      throw new NotFoundException(`Interview #${id} not found`);
+    }
+  }
+}
