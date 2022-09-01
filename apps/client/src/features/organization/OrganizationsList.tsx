ce9e51915fe9c2b980a81ea9c766f51e9437821a
Mazedul Islam <mazidmailbox@gmail.com>
2022-09-01 10:21:32 +0600

    redux and rtk query setup

--- apps/client/src/features/organization/OrganizationsList.tsx
+++ apps/client/src/features/organization/OrganizationsList.tsx
@@ -1,5 +1,31 @@
+import { Spinner } from '@chakra-ui/react';
+import { useGetOrganizationsQuery } from '../api/apiSlice';
+
 const OrganizationsList = () => {
-  return <div>OrganizationsList</div>;
+  const {
+    data: organizations,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetOrganizationsQuery(null);
+
+  let content;
+
+  if (isLoading) {
+    content = <Spinner />;
+  } else if (isSuccess) {
+    content = organizations.toString();
+  } else if (isError) {
+    content = <div>{error.toString()}</div>;
+  }
+
+  return (
+    <>
+      <div>OrganizationsList</div>
+      {content}
+    </>
+  );
 };
 
 export default OrganizationsList;
--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,22 +3,27 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import theme from './theme';
-
 import App from './App';
+import { store } from './app/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={process.env['NX_GOOGLE_CLIENT_ID'] || ''}>
-      <ChakraProvider theme={theme}>
-        <ColorModeScript initialColorMode={theme['config'].initialColorMode} />
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ChakraProvider>
-    </GoogleOAuthProvider>
+    <Provider store={store}>
+      <GoogleOAuthProvider clientId={process.env['NX_GOOGLE_CLIENT_ID'] || ''}>
+        <ChakraProvider theme={theme}>
+          <ColorModeScript
+            initialColorMode={theme['config'].initialColorMode}
+          />
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ChakraProvider>
+      </GoogleOAuthProvider>
+    </Provider>
   </StrictMode>
 );
