0a63fabd43de9f9fb427f03ee9fe6d8c25cc96b1
Mazedul Islam <mazidmailbox@gmail.com>
2022-09-06 08:33:13 +0600

    load current user on page refresh

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -7,6 +7,20 @@ import { Provider } from 'react-redux';
 import theme from './theme';
 import App from './App';
 import { store } from './app/store';
+import { authApiSlice } from './features/auth/authApiSlice';
+import { setUser } from './features/auth/authSlice';
+
+const getCurrentUser = async () => {
+  try {
+    const user = await store
+      .dispatch(authApiSlice.endpoints.currentUser.initiate(null))
+      .unwrap();
+    store.dispatch(setUser(user));
+  } catch (err) {
+    console.log(err);
+  }
+};
+getCurrentUser();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
