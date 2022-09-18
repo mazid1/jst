8a1b02ec107a0b1666025b1891009a4425a50803
Mazedul Islam <mazidmailbox@gmail.com>
2022-09-18 12:13:53 +0600

    do not redirect to root path when directly entered a nested path

--- apps/client/src/features/auth/authApiSlice.ts
+++ apps/client/src/features/auth/authApiSlice.ts
@@ -1,3 +1,4 @@
+import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query/fetchBaseQuery';
 import { apiSlice } from '../api/apiSlice';
 import { User, resetUser, setUser } from './userSlice';
 
@@ -37,15 +38,20 @@ export const authApiSlice = apiSlice.injectEndpoints({
     }),
 
     currentUser: builder.query<User, void>({
-      query: () => '/users/me',
-      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
-        try {
-          const { data: user } = await queryFulfilled;
-          dispatch(setUser(user));
-        } catch (e) {
-          console.log('failed to get current user', e);
-          dispatch(resetUser());
+      async queryFn(arg, api, extraOptions, baseQuery) {
+        const userResult = await baseQuery('/users/me');
+
+        if (userResult.data) {
+          api.dispatch(setUser(userResult.data as User));
+        } else {
+          api.dispatch(resetUser());
         }
+
+        return userResult.data
+          ? {
+              data: userResult.data as User,
+            }
+          : { error: userResult.error as FetchBaseQueryError };
       },
     }),
   }),
