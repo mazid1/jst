091eb5cfd4e76301c2c9fbcd2402b2aceeffa930
Mazedul Islam <mazidmailbox@gmail.com>
2023-01-14 22:21:57 +0600

    extract UserMenu component

--- apps/client/src/components/sidebar/Topbar.tsx
+++ apps/client/src/components/sidebar/Topbar.tsx
@@ -1,6 +1,4 @@
 import {
-  Avatar,
-  Box,
   Flex,
   FlexProps,
   HStack,
@@ -15,6 +13,7 @@ import {
   VStack,
 } from '@chakra-ui/react';
 import { FiBell, FiChevronDown, FiMenu } from 'react-icons/fi';
+import UserMenu from '../user/UserMenu';
 
 interface MobileProps extends FlexProps {
   onOpen: () => void;
@@ -56,48 +55,7 @@ const Topbar = ({ onOpen, ...rest }: MobileProps) => {
           aria-label="open menu"
           icon={<FiBell />}
         />
-        <Flex alignItems={'center'}>
-          <Menu>
-            <MenuButton
-              py={2}
-              transition="all 0.3s"
-              _focus={{ boxShadow: 'none' }}
-            >
-              <HStack>
-                <Avatar
-                  size={'sm'}
-                  src={
-                    'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                  }
-                />
-                <VStack
-                  display={{ base: 'none', md: 'flex' }}
-                  alignItems="flex-start"
-                  spacing="1px"
-                  ml="2"
-                >
-                  <Text fontSize="sm">Justina Clark</Text>
-                  <Text fontSize="xs" color="gray.600">
-                    Admin
-                  </Text>
-                </VStack>
-                <Box display={{ base: 'none', md: 'flex' }}>
-                  <FiChevronDown />
-                </Box>
-              </HStack>
-            </MenuButton>
-            <MenuList
-              bg={useColorModeValue('white', 'gray.900')}
-              borderColor={useColorModeValue('gray.200', 'gray.700')}
-            >
-              <MenuItem>Profile</MenuItem>
-              <MenuItem>Settings</MenuItem>
-              <MenuItem>Billing</MenuItem>
-              <MenuDivider />
-              <MenuItem>Sign out</MenuItem>
-            </MenuList>
-          </Menu>
-        </Flex>
+        <UserMenu />
       </HStack>
     </Flex>
   );
