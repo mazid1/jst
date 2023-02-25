4d3f726a2a7f0dd478ef3c462bfcaa4e580532c9
Mazedul Islam <mazidmailbox@gmail.com>
2023-02-25 16:28:39 +0600

    refactor: extract organizationApiSlice

--- apps/client/src/components/organization/OrganizationsList.tsx
+++ apps/client/src/components/organization/OrganizationsList.tsx
@@ -1,5 +1,5 @@
 import { Spinner } from '@chakra-ui/react';
-import { useGetOrganizationsQuery } from '../../redux/api/apiSlice';
+import { useGetOrganizationsQuery } from '../../redux/api/organizationApiSlice';
 
 const OrganizationsList = () => {
   const {
