2577587765e2d715416e8b536ddf11c02d4ab71f
Mazedul Islam <mazidmailbox@gmail.com>
2023-02-26 11:45:32 +0600

    feat: render user name and avatar properly

--- apps/client/src/components/user/UserMenu.tsx
+++ apps/client/src/components/user/UserMenu.tsx
@@ -14,9 +14,12 @@ import {
 } from '@chakra-ui/react';
 import { FiChevronDown } from 'react-icons/fi';
 import { useLogoutMutation } from '../../redux/api/authApiSlice';
+import { useAppSelector } from '../../redux/hooks';
+import { selectCurrentUser } from '../../redux/slices/userSlice';
 
 const UserMenu = () => {
   const [logout] = useLogoutMutation();
+  const user = useAppSelector(selectCurrentUser);
 
   const startLogout = async () => {
     try {
@@ -31,19 +34,14 @@ const UserMenu = () => {
       <Menu>
         <MenuButton py={2} transition="all 0.3s" _focus={{ boxShadow: 'none' }}>
           <HStack>
-            <Avatar
-              size={'sm'}
-              src={
-                'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-              }
-            />
+            <Avatar size={'sm'} src={user?.picture} />
             <VStack
               display={{ base: 'none', md: 'flex' }}
               alignItems="flex-start"
               spacing="1px"
               ml="2"
             >
-              <Text fontSize="sm">Justina Clark</Text>
+              <Text fontSize="sm">{user?.name}</Text>
               <Text fontSize="xs" color="gray.600">
                 Admin
               </Text>
