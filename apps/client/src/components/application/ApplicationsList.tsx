5d9c70537b5eb29344661d2d2bcdb1e3a491a6c0
Mazedul Islam <mazidmailbox@gmail.com>
2023-04-14 12:06:06 +0600

    feat(client): basic application table added

--- apps/client/src/components/application/ApplicationsList.tsx
+++ apps/client/src/components/application/ApplicationsList.tsx
@@ -1,5 +1,8 @@
 import { Spinner } from '@chakra-ui/react';
+import { createColumnHelper } from '@tanstack/react-table';
+import { Application } from '../../@types';
 import { useGetApplicationsQuery } from '../../redux/api/applicationApiSlice';
+import { DataTable } from '../common/DataTable';
 
 function ApplicationsList() {
   const {
@@ -16,7 +19,23 @@ function ApplicationsList() {
     content = <Spinner />;
   } else if (isSuccess) {
     console.log(applications);
-    content = JSON.stringify(applications, null, 4);
+    // content = JSON.stringify(applications, null, 4);
+    const columnHelper = createColumnHelper<Application>();
+    const columns = [
+      columnHelper.accessor('position', {
+        cell: (info) => info.getValue(),
+        header: 'Position',
+      }),
+      columnHelper.accessor('organization.name', {
+        cell: (info) => info.getValue(),
+        header: 'Company',
+      }),
+      columnHelper.accessor('status', {
+        cell: (info) => info.getValue(),
+        header: 'Status',
+      }),
+    ];
+    content = <DataTable columns={columns} data={applications} />;
   } else if (isError) {
     content = <div>{error.toString()}</div>;
   }
