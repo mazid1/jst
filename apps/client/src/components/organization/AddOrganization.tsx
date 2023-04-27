77c6fe14526907d604ccc190ba1179cd74d47241
Mazedul Islam <mazidmailbox@gmail.com>
2023-04-27 21:07:48 +0600

    feat(client): add organization dummy modal added

--- apps/client/src/components/organization/AddOrganization.tsx
+++ apps/client/src/components/organization/AddOrganization.tsx
@@ -1,11 +1,41 @@
 import { AddIcon } from '@chakra-ui/icons';
-import { Button } from '@chakra-ui/react';
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from '@chakra-ui/react';
 
 function AddOrganization() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <Button leftIcon={<AddIcon />} colorScheme="teal">
-      Add Organization
-    </Button>
+    <>
+      <Button leftIcon={<AddIcon />} colorScheme="teal" onClick={onOpen}>
+        Add Organization
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose} closeOnOverlayClick={false}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add a new organization</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>Form body will go here.</ModalBody>
+
+          <ModalFooter>
+            <Button variant="ghost" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="teal">Save Organization</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
 
