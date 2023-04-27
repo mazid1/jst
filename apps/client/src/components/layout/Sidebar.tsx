76b5229d7fa3565f72836f0bdcf3e0ca0cba8628
Mazedul Islam <mazidmailbox@gmail.com>
2023-04-27 11:11:58 +0600

    fix(client): main section background color issue

--- apps/client/src/components/layout/Sidebar.tsx
+++ apps/client/src/components/layout/Sidebar.tsx
@@ -12,7 +12,7 @@ import Topbar from './Topbar';
 const Sidebar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
+    <Box minH="100vh" bg={useColorModeValue('white', 'gray.900')}>
       <SidebarContent
         onClose={() => onClose}
         display={{ base: 'none', md: 'block' }}
