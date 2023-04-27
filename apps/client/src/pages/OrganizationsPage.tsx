76ec42db653f85776891b8dab7a3ad468e0b861d
Mazedul Islam <mazidmailbox@gmail.com>
2023-04-27 20:56:52 +0600

    refactor(client): extract add organization component

--- apps/client/src/pages/OrganizationsPage.tsx
+++ apps/client/src/pages/OrganizationsPage.tsx
@@ -1,7 +1,7 @@
-import { AddIcon } from '@chakra-ui/icons';
-import { Button, Stack } from '@chakra-ui/react';
+import { Stack } from '@chakra-ui/react';
 import { useLocation } from 'react-router-dom';
 import PageTitle from '../components/common/PageTitle';
+import AddOrganization from '../components/organization/AddOrganization';
 import OrganizationsList from '../components/organization/OrganizationsList';
 import { history } from '../helpers/history';
 
@@ -12,9 +12,7 @@ const OrganizationsPage = () => {
     <>
       <PageTitle title="Organizations" />
       <Stack direction="row" justifyContent="end" mb={6}>
-        <Button leftIcon={<AddIcon />} colorScheme="teal">
-          Add Organization
-        </Button>
+        <AddOrganization />
       </Stack>
       <OrganizationsList />
     </>
