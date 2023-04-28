463ba3255fee0450fc08dc9d752f473a1fc30099
Mazedul Islam <mazidmailbox@gmail.com>
2023-04-28 22:47:12 +0600

    feat(client): organization form validation added

--- apps/client/src/components/organization/OrganizationForm.tsx
+++ apps/client/src/components/organization/OrganizationForm.tsx
@@ -4,17 +4,29 @@ import {
   FormLabel,
   Input,
 } from '@chakra-ui/react';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { z } from 'zod';
 import { CreateOrganizationDto } from '../../@types';
 
 export const ORGANIZATION_FORM_ID = 'organizationForm';
+
+const schema = z.object({
+  name: z.string().nonempty('Name is required'),
+  location: z.string(),
+  size: z.string(),
+  minSalary: z.number().nonnegative().optional(),
+  maxSalary: z.number().nonnegative().optional(),
+  website: z.string().url().optional().or(z.literal('')),
+  linkedinPage: z.string().url().optional().or(z.literal('')),
+});
 
 function OrganizationForm() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<CreateOrganizationDto>();
+  } = useForm<CreateOrganizationDto>({ resolver: zodResolver(schema) });
 
   const onSubmit: SubmitHandler<CreateOrganizationDto> = (data) =>
     console.log(data);
@@ -24,37 +36,53 @@ function OrganizationForm() {
       <FormControl isInvalid={!!errors.name}>
         <FormLabel>Name</FormLabel>
         <Input type="text" {...register('name', { required: true })} />
-        <FormErrorMessage>Name is required.</FormErrorMessage>
+        <FormErrorMessage>{errors.name?.message}</FormErrorMessage>
       </FormControl>
 
-      <FormControl>
+      <FormControl isInvalid={!!errors.location}>
         <FormLabel>Location</FormLabel>
         <Input type="text" {...register('location')} />
+        <FormErrorMessage>{errors.location?.message}</FormErrorMessage>
       </FormControl>
 
-      <FormControl>
+      <FormControl isInvalid={!!errors.size}>
         <FormLabel>Size</FormLabel>
         <Input type="text" {...register('size')} />
+        <FormErrorMessage>{errors.size?.message}</FormErrorMessage>
       </FormControl>
 
-      <FormControl>
+      <FormControl isInvalid={!!errors.minSalary}>
         <FormLabel>Minimum Salary</FormLabel>
-        <Input type="text" {...register('minSalary')} />
+        <Input
+          type="number"
+          {...register('minSalary', {
+            setValueAs: (v) => (v ? parseFloat(v) : undefined),
+          })}
+        />
+        <FormErrorMessage>{errors.minSalary?.message}</FormErrorMessage>
       </FormControl>
 
-      <FormControl>
+      <FormControl isInvalid={!!errors.maxSalary}>
         <FormLabel>Maximum Salary</FormLabel>
-        <Input type="text" {...register('maxSalary')} />
+        <Input
+          type="number"
+          {...register('maxSalary', {
+            setValueAs: (v) => (v ? parseFloat(v) : undefined),
+          })}
+        />
+        <FormErrorMessage>{errors.maxSalary?.message}</FormErrorMessage>
       </FormControl>
 
-      <FormControl>
+      <FormControl isInvalid={!!errors.website}>
         <FormLabel>Website</FormLabel>
-        <Input type="text" {...register('website')} />
+        <Input type="url" {...register('website')} />
+        <FormErrorMessage>{errors.website?.message}</FormErrorMessage>
       </FormControl>
 
-      <FormControl>
+      <FormControl isInvalid={!!errors.linkedinPage}>
         <FormLabel>Linkedin</FormLabel>
-        <Input type="text" {...register('linkedinPage')} />
+        <Input type="url" {...register('linkedinPage')} />
+        <FormErrorMessage>{errors.linkedinPage?.message}</FormErrorMessage>
       </FormControl>
     </form>
   );
