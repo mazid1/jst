49bb9a2b3882f3a0a48d8b274afadf61770eca8e
Mazedul Islam <mazidmailbox@gmail.com>
2023-06-17 11:02:00 +0600

    test(api): removed unnecessary files and refactored app module

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,7 +8,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 
-import { AppModule } from './app/app.module';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
